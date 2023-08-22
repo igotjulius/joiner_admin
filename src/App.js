ac9450807bd35df46d5a1c69aa5d6c8bdf37ea1b
Julius Wilmar <wilmarjulius@gmail.com>
2023-08-23 01:19:39 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="left-container">
+        <h1>JOINER</h1>
+        <div className="content">
+          <div className="section">
+            <h2>Discover Unseen Beauty</h2>
+            <p>Your Adventure Awaits!</p>
+          </div>
+          <div className="section">
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <p className="forgot-password">Forgot Password?</p>
+          </div>
+        </div>
+        <button>Login</button>
+        <p className="footer">Copyright 2023</p>
+      </div>
+      <div className="right-container">
+        <p className="quote">“Experience is a wonderful thing.”</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './Dashboard.css';
+
+const Dashboard = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabChange = (tabIndex) => {
+    setSelectedTab(tabIndex);
+  };
+
+  return (
+    <div className="dashboard-container">
+      <div className="sidebar">
+        <h2>Joiner</h2>
+        <div className="tabs">
+          <div
+            className={`tab ${selectedTab === 0 ? 'selected' : ''}`}
+            onClick={() => handleTabChange(0)}
+          >
+            <div className="tab-icon">
+            </div>
+            <div className="tab-label">Dashboard</div>
+          </div>
+          <div
+            className={`tab ${selectedTab === 1 ? 'selected' : ''}`}
+            onClick={() => handleTabChange(1)}
+          >
+            <div className="tab-icon">
+            </div>
+            <div className="tab-label">Transactions</div>
+          </div>
+          <div
+            className={`tab ${selectedTab === 2 ? 'selected' : ''}`}
+            onClick={() => handleTabChange(2)}
+          >
+            <div className="tab-icon">
+            </div>
+            <div className="tab-label">Rentals</div>
+          </div>
+          <div className="tab">
+            <div className="tab-icon">
+            </div>
+            <div className="tab-label">Logout</div>
+          </div>
+        </div>
+      </div>
+      <div className="main-content">
+        <div className="content-header">
+          <h2>Dashboard</h2>
+        </div>
+        <hr className="divider" />
+        <div className="tab-content">
+          {selectedTab === 0 && (
+            <div className="tab-image">
+              <img src={require('./Frame_96_(1).png')} alt="Number of Users" />
+              <img src={require('./Frame_97.png')} alt="Transactions" />
+            </div>
+          )}
+          {selectedTab === 1 && (
+            <div className="tab-image">
+              <img src={require('./Frame_96_(2).png')} alt="Transaction History" />
+            </div>
+          )}
+          {selectedTab === 2 && (
+            <div className="tab-image">
+              <img src={require('./Frame_96_(3).png')} alt="Rentals" />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
